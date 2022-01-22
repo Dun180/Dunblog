3f42ea219792112501acd649c474de2b1c885e60
Dun180 <74287755+Dun180@users.noreply.github.com>
2022-01-22 23:43:21 +0800

    first commit

---
+++ dunblog-spring/src/main/java/com/dun/DunblogSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunblogSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DunblogSpringApplication.class, args);
+    }
+
+}
